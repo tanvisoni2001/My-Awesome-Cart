453b4ce9611e4d36def2c2e89141f677d315d41c
Tanvi Soni <tanvisony181@gmail.com>
2024-02-22 11:39:17 +0530

    inherit templates

--- mac/settings.py
+++ mac/settings.py
@@ -116,8 +116,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'shop/static/shop')
+] 
 
-STATIC_URL = 'static/'
+#managing media
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- shop/views.py
+++ shop/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'shop/index.html')
+    products = Product.objects.all()
+    n = len(products)
+    nSlides = n//4 + ceil((n/4) - (n//4))
+    params = {"product":products, 'nSlides':nSlides, 'range':range(1,nSlides)}
+    return render(request, 'shop/index.html', params)
 
 def about(request):
-    pass 
+    return render(request, 'shop/aboutUs.html')
 
 def contact(request):
     pass 
